82591d992210b4919b289a890308c1449287f798
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-03-18 00:26:59 -0300

    desafio

--- desafios/desafiobola.c
+++ desafios/desafiobola.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int n, n2, x = 0, x1 = 0, x3 = 0, cont = 0;
+    int n, n2, x = 0, x1 = 0, cont = 0;
     printf("Digite um numero inteiro: ");
     scanf("%i", &n);
     n2 = n;
-    while(x3 <= n){
+    while(0 != n){
         while(x < n){
             while (x1 < n2){  
+                printf("*");
                 x1++;
                 cont++;
             }
+            printf("\n");
             x1 = 0;
             n2 = n2 - 1;
             x++;
         }
-        x3++;
+        printf("\n");
         n = n - 1;
-        n2 = n2 - 1;
+        n2 = n;
         x = 0;
         x1 = 0;
-        printf("t ");
     }
-    printf("%i", cont);
+    printf("O total de bolas de canhao é %i\n", cont);
 }
