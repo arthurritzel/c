333f75e9308d0b5a4612f516538e1055fa06ce8f
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-05-06 16:01:04 -0300

    exe11

--- lista 7 vetor/exe011.c
+++ lista 7 vetor/exe011.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    
+    int n[10], very, n2[10], j = 0;
+    for (int i = 0; i < 10; i++){
+        do{
+            very = 0;
+            printf("->");
+            scanf("%i", &n[i]);
+
+            
+                if(n[i] > 50 || n[i] < 0){
+                    very = 1;
+                    printf("Digite no intervalo de 0 a 50\n");
+                    break;
+                }else{
+                    very = 0;
+                    if(n[i] % 2 == 1){
+                        n2[j] = n[i];
+                        j++;
+                    }else{
+                        n2[j] = 0;
+                        j++;
+                    }
+                }
+            
+        }while (very != 0);
+    }
+
+    for (int i = 0; i < 10; i++){
+        printf("%i\n", n2[i]);
+    }
 }
