b51adfc8a12526ccb2a374a2f33d8d6e2d5a446e
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-05-10 01:12:22 -0300

    ajustes

--- lista 5 for/exe011.c
+++ lista 5 for/exe011.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int b, n;
-    
+    int b, n, f = 0;
+    printf("Digite o valor de b: ");
+    scanf("%i", &b);
+    printf("Digite o valor de n: ");
+    scanf("%i", &n);
+
+    for (int i = 1; i < n; i++){
+        f += b * b;
+    }
+    printf("%i", f);
 }
